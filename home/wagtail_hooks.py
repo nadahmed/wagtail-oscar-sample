4481bca73eb06739846730cc4d8304af38b95dde
Noor A <nooraldinahmed@gmail.com>
2024-02-01 01:33:26 +0600

    shop link added to wagtail userbar

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -6,3 +6,23 @@ from wagtail.admin.menu import MenuItem
 @hooks.register('register_admin_menu_item')
 def register_shop_dashboard_menu_item():
   return MenuItem('Shop Dashboard', reverse("dashboard:index"), icon_name='link-external', order=0, attrs={'target': '_blank'})
+
+class UserbarShopLinkItem:
+    def render(self, request):
+        from django.template import Template, Context
+        doc = '''
+        <li class="wagtail-userbar__item " role="presentation">
+          <a href="{% url 'dashboard:index' %}" target="_blank" role="menuitem" tabindex="-1">
+            <svg class="icon icon-link-external wagtail-action-icon" aria-hidden="true">
+            {% include "wagtailadmin/icons/link-external.svg" %}</svg>
+                Go to Shop Dashboard
+          </a>
+        </li>
+        '''
+        t = Template(doc)
+        return t.render(Context({}))
+
+@hooks.register('construct_wagtail_userbar')
+def add_shop_dashboard_link_item(request, items:list=[]):
+    items.insert(1, UserbarShopLinkItem())
+    return items
